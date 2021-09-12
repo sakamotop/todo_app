b668b2fdfd8f098ca9bf1a1d6631ae310b0d2ede
sakamotop <hito_k@outlook.jp>
2021-09-13 00:50:29 +0900

    add scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int test = 0;
 
-  void _incrementCounter() {
+  //final todolist = List<String>.generate(10, (i) => "Item $i");
+
+  void _test() {
     setState(() {
-      _counter++;
+      test++;
     });
   }
 
@@ -45,25 +47,47 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: SingleChildScrollView(
+          // SingleChildScrollViewで子ウィジェットをラップ
+          scrollDirection: Axis.horizontal,
+          // スクロールの向きを水平方向に指定
+          child: Row(
+            children: [
+              myContainer(
+                color: Colors.blue,
+                text: 'RUN',
+              ),
+              myContainer(color: Colors.orange, text: 'SHOPPING'),
+              myContainer(color: Colors.red, text: 'WORK'),
+              myContainer(color: Colors.blue, text: 'LUNCH'),
+              myContainer(color: Colors.orange, text: 'DINNER'),
+              myContainer(color: Colors.red, text: 'SLEEP'),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _test,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget myContainer({double size = 300, Color? color, String text = ''}) {
+    return Container(
+      color: color,
+      width: size,
+      height: size,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+              fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
     );
   }
 }
